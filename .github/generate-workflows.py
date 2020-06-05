968030c56d6dc1723e731ba503184d43571f5444
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-06-05 13:29:21 +0200

    add workflow generator

---
+++ .github/generate-workflows.py
@@ -0,0 +1,17 @@
+import yaml
+from copy import copy
+
+
+with open("./spack-config/spack.yaml") as f:
+    spack = yaml.safe_load(f)
+    specs = spack["spack"]["specs"][:]
+
+with open("./template/package-test.template.yaml") as f:
+    template = f.readlines()
+
+for spec in specs:
+    workflow = copy(template)
+    workflow = [l.format(PACKAGE=spec) for l in workflow]
+
+    with open(f"./workflows/individual/package-test.{spec}.yaml", 'w') as f:
+        f.writelines(workflow)
