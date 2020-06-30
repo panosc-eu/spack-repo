0afb838e00f54e78c263ed5150a42188d083027a
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-06-30 15:50:31 +0200

    Add new versions from pypi (missing GH release)

--- packages/py-msgpack-numpy/package.py
+++ packages/py-msgpack-numpy/package.py
@@ -37,6 +37,10 @@ class PyMsgpackNumpy(PythonPackage):
 
     maintainers = ['robertrosca']
 
+    version('0.4.6.post0', sha256='dfcb0c9cb5850e656344ac464a260e7b8b9b1c62d77c2e1d3d9ef15a88f1df6b',
+        url='https://files.pythonhosted.org/packages/86/ee/9e0fa0979a3aee96b3e2efe86f6253a2527c8faac1d8c1c48aa5e977ca3d/msgpack-numpy-0.4.6.post0.tar.gz')
+    version('0.4.6',   sha256='ef3c5fe3d6cbab5c9db97de7062681c18f82d32a37177aaaf58b483d0336f135',
+        url='https://files.pythonhosted.org/packages/14/c7/9140e75f2c9dfbd1423500fe7d3652c20a721919c63e74cadcc09276b25a/msgpack-numpy-0.4.6.tar.gz')
     version('0.4.5',   sha256='4e88a4147db70f69dce1556317291e04e5107ee7b93ea300f92f1187120da7ec')
     version('0.4.4.3', sha256='c7db37ce01e268190568cf66a6a65d1ad81e3bcfa55dd824103c9b324608a44e')
     version('0.4.4.2', sha256='ac3db232710070ac64d8e1c5123550a1c1fef45d77b6789d2170cbfd2ec711f3')
