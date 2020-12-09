9dbd1b2536de37d5e444b464167bd19ddd6bbac5
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-12-09 09:42:57 +0100

    Fix typo, call self.pytest

--- packages/py-extra-geom/package.py
+++ packages/py-extra-geom/package.py
@@ -63,7 +63,7 @@ class PyExtraGeom(PythonPackage):
         # `setup.py test` should not be used as:
         #   - `python3 -m pytest -v` should be ran instead
         #   - the builtin `test` method runs before `install` is finished
-        pytest(self)
+        self.pytest(self)
 
     def pytest(self):
         with working_dir('.'):
