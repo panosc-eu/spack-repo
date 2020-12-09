23e1e7cef6f3257ef4575e2d0d48a7cbe7eb4d08
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-12-09 09:39:31 +0100

    Revert accidental changes

--- packages/py-extra-data/package.py
+++ packages/py-extra-data/package.py
@@ -11,11 +11,11 @@
 # next to all the things you'll want to change. Once you've handled
 # them, you can save this file and test your package like this:
 #
-#     spack install py-extra-geom
+#     spack install py-extra-data
 #
 # You can edit this file again by typing:
 #
-#     spack edit py-extra-geom
+#     spack edit py-extra-data
 #
 # See the Spack documentation for more information on packaging.
 # ----------------------------------------------------------------------------
@@ -23,38 +23,36 @@
 from spack import *
 
 
-class PyExtraGeom(PythonPackage):
-    """Tools to work with EuXFEL detector geometry and assemble detector images."""
+class PyExtraData(PythonPackage):
+    """Tools to read and analyse data from European XFEL."""
 
     # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://github.com/European-XFEL/EXtra-geom"
-    url      = "https://github.com/European-XFEL/EXtra-geom/archive/0.9.0.tar.gz"
+    homepage = "https://github.com/European-XFEL/EXtra-data"
+    url      = "https://github.com/European-XFEL/EXtra-data/archive/1.2.0.tar.gz"
 
     # FIXME: Add a list of GitHub accounts to
     # notify when the package is updated.
-    maintainers = ['github_user1', 'github_user2']
+    maintainers = ['robertrosca']
     install_time_test_callbacks = ['import_module_test']
 
-    version('1.0.0',  sha256='c59a0301ccab81b541717e406e1b97844eb450c10df5e031b417b87987d90deb')
-    version('1.0.0',  sha256='5d3d6c275b4f3b734005d5eb6ec1fb77361fdf386ae1e973859c7deb7e2dea81')
-    version('0.10.0', sha256='01d1bb2edf5c6b624f3d598833e2729fe108f53991e2a9c58588ae0719295a10')
-    version('0.9.0', sha256='3a757a1517d016ca95f438c5aa9383dc1eefe45a242e651fe54ef4308324e41d')
-    version('0.8.0', sha256='329d3addec5d992f592720d93242efcf2d1e1272917de3207408033e1657d21d')
-    version('0.7.0', sha256='035986e7875301cabd0e1cfef7c31fa359fb92755e89507421bbf8db8c76b8d6')
-    version('0.6.2', sha256='a210803286cbd3930830d38cc75fa8c591c7c9f3e64ebdb3df752f0d746092b6')
-    version('0.6.1', sha256='a31943bdef21c740c94cbeebde8ec9f1348f5596c28ed639eae5603ef1074775')
-    version('0.6.0', sha256='17ca1939117016f6337c5f4051c4222ca47192c6fc004835e85e782b4ee8653f')
+    version('1.3.0', sha256='7150b0d7ec3ea97af9e0b81bd88aeddb0716e79b852c603a83f4f04ff65935ce')
+    version('1.2.0', sha256='bdb1da5469d314dc1c22cbbd1ecc6e1c9e0660bd1bada4f9a8efd97b3d8b1a0e')
 
     # FIXME: Add dependencies if required.
     depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
-    depends_on('py-cfelpyutils@0.92:')
+    depends_on('py-fabio') #  custom
     depends_on('py-h5py@2.7.1:')
+    depends_on('py-karabo-bridge@0.6:') #  custom
     depends_on('py-matplotlib')
     depends_on('py-numpy')
+    depends_on('py-pandas')
     depends_on('py-scipy')
+    depends_on('py-xarray')
 
-    depends_on('py-coverage@:4.9', type='test')
+    depends_on('py-coverage', type='test')
+    depends_on('py-dask', type='test')
+    # depends_on ('py-nbval', type='test') #  Doesn't seem to be needed?
     depends_on('py-pytest', type='test')
     depends_on('py-pytest-cov', type='test')
     depends_on('py-testpath', type='test')
@@ -63,8 +61,9 @@ class PyExtraGeom(PythonPackage):
         # `setup.py test` should not be used as:
         #   - `python3 -m pytest -v` should be ran instead
         #   - the builtin `test` method runs before `install` is finished
-        pytest(self)
+        pass
 
+    @run_after('install')
     def pytest(self):
         with working_dir('.'):
             prefix = self.spec.prefix
