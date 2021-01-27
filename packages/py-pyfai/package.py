a77e48419597c5fb926054d6f6b2fd0f8c66b45a
Julian Michael Hoersch <julian.hoersch@xfel.eu>
2021-01-27 10:59:58 +0100

    Added v0.20.0

--- packages/py-pyfai/package.py
+++ packages/py-pyfai/package.py
@@ -32,8 +32,9 @@ class PyPyfai(PythonPackage):
 
     # FIXME: Add a list of GitHub accounts to
     # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
+    maintainers = ['RobertRosca', 'julianhoersch']
 
+    version('0.20.0', sha256='f39f56ff58868ab7f12d6fa5c7bf6b7d111b7fa687f3816c5d8d11cb75fa82f0')
     version('0.19.0', sha256='1e329d2bdd6f4ddc4c70460bb0b994a33b6885bff810b11a7871d83ef8756281')
     version('0.18.0', sha256='4637eaa40a5a3b10c97ef0fea9e6f935c4ae93eb8df48643d567db7796cb3ef8')
     version('0.17.1', sha256='e4c2b53c71b638d5f532f5e557a87a79d1a3ab46552e4b5ad0f071e996f3974d')
@@ -50,7 +51,7 @@ class PyPyfai(PythonPackage):
     depends_on('py-h5py')
     depends_on('py-silx')
     depends_on('py-pyopengl')
-    depends_on('py-pyqt5')
+    depends_on('py-pyqt5') 
     depends_on('py-hdf5plugin')
     depends_on('py-wheel')
 
