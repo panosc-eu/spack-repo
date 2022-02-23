bd26690a046c66b55d795d289d98d89744d476e3
Julian Michael Hoersch <julian.hoersch@xfel.eu>
2022-02-23 08:21:35 +0100

    removing boilerplate, change wdir of test

--- packages/py-numexpr/package.py
+++ packages/py-numexpr/package.py
@@ -3,47 +3,26 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-numexpr
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-numexpr
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyNumexpr(PythonPackage):
-    """NumExpr is a fast numerical expression evaluator for NumPy. With it, 
-    expressions that operate on arrays (like '3*a+4*b') are accelerated and 
-    use less memory than doing the same calculation in Python. 
-    
-    In addition, its multi-threaded capabilities can make use of all your cores 
-    -- which generally results in substantial performance scaling compared to NumPy. 
-    
-    Last but not least, numexpr can make use of Intel's VML (Vector Math Library, 
-    normally integrated in its Math Kernel Library, or MKL). This allows further 
+    """NumExpr is a fast numerical expression evaluator for NumPy. With it,
+    expressions that operate on arrays (like '3*a+4*b') are accelerated and
+    use less memory than doing the same calculation in Python.
+
+    In addition, its multi-threaded capabilities can make use of all your cores
+    -- which generally results in substantial performance scaling compared to NumPy.
+
+    Last but not least, numexpr can make use of Intel's VML (Vector Math Library,
+    normally integrated in its Math Kernel Library, or MKL). This allows further
     acceleration of transcendent expressions."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/pydata/numexpr/"
     url      = "https://github.com/pydata/numexpr/archive/v2.7.2.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
     maintainers = ['RobertRosca', 'julianhoersch']
 
-    version('3.0.1a5', sha256='e2ff7f3244a6c780059b7d80f22af07bbf93b01502da0df10c553fb30c6e8de2')
     version('2.7.2',   sha256='7d1b3790103221feda07f4a93a4fa5c6654f46865197a677ca6f27eb5cb4e5ef')
     version('2.7.1',   sha256='5c6ae3bb5688184b922b43fc47de49d642576d0feec55a1b679caa66efae90a1')
     version('2.7.0',   sha256='1923f038b90cc69635871968ed742be7775c879451c612f173c2547c823c9561')
@@ -54,23 +33,12 @@ class PyNumexpr(PythonPackage):
     version('2.6.5',   sha256='fe78a78e002806e87e012b6105f3b3d52d47fc7a72bafb56341fcec7ce02cfd7')
     version('2.6.4',   sha256='049da1c07bd62d2aba29887130ccc9aff9b90962cb779a7b7ddc15e580368fba')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    depends_on('python@2.6:', type=('build', 'run')) 
+
+    depends_on('python@2.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-numpy@1.7:')
 
-    # test dependencies
-    depends_on('py-coverage@:4.9', type='test')
-    depends_on('py-pytest', type='test')
-    depends_on('py-pytest-cov', type='test')
-    depends_on('py-testpath', type='test')
-
-    install_time_test_callbacks = ['import_module_test']
-    
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    def test(self):
+        prefix = self.spec.prefix
+        with working_dir(prefix):
+            python('-c', 'import numexpr; numexpr.test()')
