85bb188bd71ad15b008c21e86ea24282e1615f3a
Julian Michael Hoersch <julian.hoersch@xfel.eu>
2022-02-23 08:38:16 +0100

    removed boilerplate, unnecessary imports, cleanup

--- packages/py-transformations/package.py
+++ packages/py-transformations/package.py
@@ -3,60 +3,26 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-transformations
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-transformations
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyTransformations(PythonPackage):
-    """Transformations is a Python library for calculating 4x4 matrices for translating, rotating, 
-    reflecting, scaling, shearing, projecting, orthogonalizing, and superimposing arrays of 3D 
-    homogeneous coordinates as well as for converting between rotation matrices, Euler angles, and 
-    quaternions. Also includes an Arcball control object and functions to decompose transformation 
+    """Transformations is a Python library for calculating 4x4 matrices for translating, rotating,
+    reflecting, scaling, shearing, projecting, orthogonalizing, and superimposing arrays of 3D
+    homogeneous coordinates as well as for converting between rotation matrices, Euler angles, and
+    quaternions. Also includes an Arcball control object and functions to decompose transformation
     matrices."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/cgohlke/transformations"
     url      = "https://github.com/cgohlke/transformations/archive/v2020.1.1.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
     maintainers = ['RobertRosca', 'julianhoersch']
 
+    version('2021.6.6',  sha256='7e61af64c46137f18db601e54a71263946e08e2d107434ce8e1edbb92163a10f')
     version('2020.1.1',  sha256='cd9cd29ba7f0ec4c109293fe5007cf632e9f5b8107acba5572672002e72860a6')
     version('2019.4.22', sha256='5686de316e2f65a67788de25ab180995429283c531814815ce4508de0df96c9a')
     version('2019.2.20', sha256='5cb940b9df13707a88bbffffdd2cb0347afa1329b7770a8350d9d4415b00f46d')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-setuptools@18.0:', type='build')
     depends_on('py-numpy@1.14.5:')
-
-    # test dependencies
-    depends_on('py-coverage@:4.9', type='test')
-    depends_on('py-pytest', type='test')
-    depends_on('py-pytest-cov', type='test')
-    depends_on('py-testpath', type='test')
-            
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
